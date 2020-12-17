bcdae9e0648f9441d458dda609211771eb4978ab
Laurie <laurie@gatsbyjs.com>
2020-12-17 12:56:33 -0500

    Add images to posts

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -2,12 +2,15 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const BlogPostPage = ({ data }) => {
   const post = data.mdx
+  const image = getImage(post.frontmatter.image)
   return (
     <Layout>
       <article>
+        <GatsbyImage image={image} alt={post.frontmatter.imageAlt} />
         <h2>{post.frontmatter.title}</h2>
         <MDXRenderer>{post.body}</MDXRenderer>
       </article>
@@ -20,6 +23,12 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
+        image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED)
+          }
+        }
+        imageAlt
       }
       body
     }
