8d6dad6e3b9f641f4fc246378baa2f001f094e5c
Laurie <laurie@gatsbyjs.com>
2020-12-17 11:47:05 -0500

    List every post on the main page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,36 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, Link } from "gatsby"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const posts = data.allMdx.nodes
+
   return (
     <Layout>
       <SEO title={`Home Page`} />
-      <div>Hello World</div>
+      {posts.map((post) => {
+        return (
+          <Link to={post.slug} key={post.slug}>
+            <h2>{post.frontmatter.title}</h2>
+          </Link>
+        )
+      })}
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query {
+    allMdx(sort: { fields: [frontmatter___title], order: ASC }) {
+      nodes {
+        slug
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
