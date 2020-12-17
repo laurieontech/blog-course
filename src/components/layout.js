2e4c72c3f0921c65453b5443596a0696f5757de8
Laurie <laurie@gatsbyjs.com>
2020-12-17 12:21:40 -0500

    Add Theme-ui and style layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,38 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import { Link } from "gatsby"
 
+const bodyStyles = {
+  mx: `2rem`,
+  padding: `1rem`,
+}
+
+const headerStyles = {
+  paddingLeft: `10px`,
+}
+
+const mainStyles = {
+  maxWidth: `container`,
+  padding: `1rem`,
+  mx: `auto`,
+  textAlign: `center`,
+}
+
+const footerStyles = {
+  textAlign: `center`,
+  background: `lightgrey`,
+}
+
 const Layout = ({ children }) => (
-  <div>
-    <header>
+  <div sx={bodyStyles}>
+    <header sx={headerStyles}>
       <h1>
         <Link to="/">Home</Link>
       </h1>
     </header>
-    <main>{children}</main>
+    <main sx={mainStyles}>{children}</main>
     <footer>
-      <p>A fun footer</p>
+      <p sx={footerStyles}>A fun footer</p>
     </footer>
   </div>
 )
