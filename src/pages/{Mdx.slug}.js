adf5eb77ea77bb6aa5b0458a5d58a6d7a6086273
Laurie <laurie@gatsbyjs.com>
2020-12-17 11:41:44 -0500

    Use MDXRenderer to display post

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -1,7 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../components/layout"
 
-const BlogPostPage = ({ data }) => <h2>{data.mdx.frontmatter.title}</h2>
+const BlogPostPage = ({ data }) => {
+  const post = data.mdx
+  return (
+    <Layout>
+      <article>
+        <h1>{post.frontmatter.title}</h1>
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </article>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query BlogPostById($id: String) {
@@ -9,6 +21,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      body
     }
   }
 `
